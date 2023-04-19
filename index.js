5d43d8055936305f3f8e88c08a6fcb7e2919930a
Tugba Esat <tugbaesat007@gmail.com>
2023-04-19 14:13:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  const matchingDrivers = drivers.filter(function (item) {
+    return item.toUpperCase() === name.toUpperCase();
+  });
+  return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, str) {
+  const matchingDrivers = drivers.filter(function (item) {
+    return item.slice(0, str.length).toUpperCase() === str.toUpperCase();
+  });
+  return matchingDrivers;
+}
+
+function matchName(drivers, str) {
+  const matchingDrivers = drivers.filter(function (item) {
+    return item.name.slice(0, str.length).toUpperCase() === str.toUpperCase();
+  });
+  return matchingDrivers;
+}
